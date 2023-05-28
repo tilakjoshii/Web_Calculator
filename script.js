6006e76c4fae3e35ef8dbddde9e70d76f7d4ad4c
Tilak Joshi <126889863+tilakjoshii@users.noreply.github.com>
2023-05-28 14:48:17 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// add natural number
+const sumOfSquareNum = (num) => {
+  const S = (num * (num + 1) * (2 * num + 1) / 6)
+
+  alert(`The result of sum of 50 is ${S} where input is ${num}`)
+}
+console.log(sumOfSquareNum(5));
+
